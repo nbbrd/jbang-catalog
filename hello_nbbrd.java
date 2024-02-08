72b24722741a4a61fa63a153bda15a381d3b8274
jreleaserbot <jreleaser@kordamp.org>
2024-02-08 15:24:19 +0000

    hello-nbbrd v2.3.4

--- hello_nbbrd.java
+++ hello_nbbrd.java
@@ -1,7 +1,7 @@
 //usr/bin/env jbang "$0" "$@" ; exit $?
 //JAVA 8+
 //DEPS com.github.nbbrd.sandbox:sandbox-cli:2.3.4
-// Generated with JReleaser 1.10.0 at 2024-02-08T15:12:06.138801686Z
+// Generated with JReleaser 1.10.0 at 2024-02-08T15:24:07.629353777Z
 
 public class hello_nbbrd {
     public static void main(String... args) throws Exception {
