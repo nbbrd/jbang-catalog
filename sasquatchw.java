5c156cabf8f46324d330fe544d78ccef196bc84a
jreleaserbot <jreleaser@kordamp.org>
2024-09-03 09:54:58 +0000

    sasquatchw v1.1.0

---
+++ sasquatchw.java
@@ -0,0 +1,10 @@
+//usr/bin/env jbang "$0" "$@" ; exit $?
+//JAVA 8+
+//DEPS com.github.nbbrd.sasquatch:sasquatch-desktop:1.1.0
+// Generated with JReleaser 1.14.0 at 2024-09-03T09:54:44.624031195Z
+
+public class sasquatchw {
+    public static void main(String... args) throws Exception {
+        sasquatch.desktop.SasTableViewer.main(args);
+    }
+}
