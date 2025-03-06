530b6b4b739c986ef7ca0a84fca2a530f88edf79
jreleaserbot <jreleaser@kordamp.org>
2025-03-06 10:45:31 +0000

    sasquatch v1.1.1

--- sasquatch.java
+++ sasquatch.java
@@ -1,7 +1,7 @@
 //usr/bin/env jbang "$0" "$@" ; exit $?
 //JAVA 8+
-//DEPS com.github.nbbrd.sasquatch:sasquatch-cli:1.1.0
-// Generated with JReleaser 1.14.0 at 2024-09-03T09:54:44.624031195Z
+//DEPS com.github.nbbrd.sasquatch:sasquatch-cli:1.1.1
+// Generated with JReleaser 1.17.0 at 2025-03-06T10:45:18.748861639Z
 
 public class sasquatch {
     public static void main(String... args) throws Exception {
