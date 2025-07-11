a8f631b9057aa57654a5d9a1dabd813a5448d2fe
jreleaserbot <jreleaser@kordamp.org>
2025-07-11 10:18:41 +0000

    hello-nbbrd-picocli v2.6.0

--- hello_nbbrd_picocli.java
+++ hello_nbbrd_picocli.java
@@ -1,7 +1,7 @@
 //usr/bin/env jbang "$0" "$@" ; exit $?
 //JAVA 8+
-//DEPS com.github.nbbrd.sandbox:sandbox-picocli:2.5.2
-// Generated with JReleaser 1.18.0 at 2025-05-19T13:39:16.869831384Z
+//DEPS com.github.nbbrd.sandbox:sandbox-picocli:2.6.0
+// Generated with JReleaser 1.19.0 at 2025-07-11T10:17:50.157033096Z
 
 public class hello_nbbrd_picocli {
     public static void main(String... args) throws Exception {
