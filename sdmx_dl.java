fc2b197d847668eb8809cfc4cd52000a1bc34820
jreleaserbot <jreleaser@kordamp.org>
2026-05-22 15:17:13 +0000

    sdmx-dl v3.0.0

--- sdmx_dl.java
+++ sdmx_dl.java
@@ -1,7 +1,7 @@
 //usr/bin/env jbang "$0" "$@" ; exit $?
 //JAVA 8+
-//DEPS com.github.nbbrd.sdmx-dl:sdmx-dl-cli:3.0.0-beta.18
-// Generated with JReleaser 1.22.0 at 2026-02-17T12:47:09.552724701Z
+//DEPS com.github.nbbrd.sdmx-dl:sdmx-dl-cli:3.0.0
+// Generated with JReleaser 1.24.0 at 2026-05-22T15:16:57.500458623Z
 
 public class sdmx_dl {
     public static void main(String... args) throws Exception {
