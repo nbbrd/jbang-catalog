5589fe780fae35fed4741f8807a38ec22dd5df9c
jreleaserbot <jreleaser@kordamp.org>
2026-06-17 10:18:04 +0000

    heylogs v0.19.1

--- heylogs.java
+++ heylogs.java
@@ -1,7 +1,7 @@
 //usr/bin/env jbang "$0" "$@" ; exit $?
 //JAVA 8+
-//DEPS com.github.nbbrd.heylogs:heylogs-cli:0.18.1
-// Generated with JReleaser 1.24.0 at 2026-05-26T08:25:35.385028075Z
+//DEPS com.github.nbbrd.heylogs:heylogs-cli:0.19.1
+// Generated with JReleaser 1.24.0 at 2026-06-17T10:17:52.695807781Z
 
 public class heylogs {
     public static void main(String... args) throws Exception {
